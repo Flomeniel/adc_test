df2f4c029f5df06e93714e412f9e4a08d9dd33ce
Flomeniel <gabriel.aygalenq@wanadoo.fr>
2014-11-04 15:39:08 +0100

    Revert "modif n°2 et ajout des fichiers manquant"
    This reverts commit 1a0ebdb85378d2899ece98f01acb096d4b9edffb.

--- FC_adc_stm32f4.h
+++
@@ -1,7 +0,0 @@
-#include "stm32f4xx_adc.h"
-#include "stm32f4xx_gpio.h"
-#include "stm32f4xx_rcc.h"
-
-void adc_configure(ADC_TypeDef* ADCx, uint32_t ADC_Chanel_x, uint32_t ADC_SampleTime_x, uint32_t GPIO_T, uint32_t GPIO_nb, uint32_t ADC_Resolution_nb);
-
-int adc_convert();
